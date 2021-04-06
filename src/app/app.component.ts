cd84870dc39ef571607207fe8a045b12404ffff1
Laura Fernandes <lauqfernandes@gmail.com>
2021-04-06 10:31:11 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'Tour of Heroes';
+  subTitle = 'My subTitle' 
 }
